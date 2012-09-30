c975b8c709e981acc6a60227a2db3fcb0f10360f
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2012-09-29 21:51:58 -0400

    Each user gets their own list

--- web_view/app/controllers/items_controller.rb
+++ web_view/app/controllers/items_controller.rb
@@ -1,21 +1,23 @@
 class ItemsController < ApplicationController
-  @@checklist = Checklist.new []
+  @@checklists = {}
 
   def index
-    @items = @@checklist.items
-    @checked_items = @@checklist.checked_items
+    @@checklists[current_email] ||= Checklist.new([])
+    checklist = @@checklists[current_email]
+    @items = checklist.items
+    @checked_items = checklist.checked_items
   end
 
   def new
   end
 
   def create
-    @@checklist = @@checklist.add_item(params[:item])
+    @@checklist[current_email] = @@checklist[current_email].add_item(params[:item])
     redirect_to items_path
   end
 
   def check
-    @@checklist = @@checklist.check_off_item(params[:item])
+    @@checklist[current_email] = @@checklist[current_email].check_off_item(params[:item])
     redirect_to items_path
   end
 end
